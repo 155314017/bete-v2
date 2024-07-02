3de9a2e48f717032ed37d8bd3815d13c6617ea63
Dito <dhito.pb97@gmail.com>
2024-07-02 11:36:11 +0700

    main

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")({
+    chart: true,
+  })],
+};
